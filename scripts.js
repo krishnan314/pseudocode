68e9a65a0848dcc4863f2b10d5ccc1f6b05abbfb
krishnan <krishnan@onlinedegree.iitm.ac.in>
2022-06-07 13:19:34 +0530

    UI added. Parser for foreach,list concatenation

---
+++ scripts.js
@@ -0,0 +1,423 @@
+var dataset = {
+  scores: [
+    {
+      CardNo: 0,
+      Name: "Bhuvanesh",
+      Gender: "M",
+      DateOfBirth: "7 Nov",
+      CityTown: "Erode",
+      Mathematics: 68,
+      Physics: 64,
+      Chemistry: 78,
+      Total: 210,
+      Average: 70,
+    },
+    {
+      CardNo: 1,
+      Name: "Harish",
+      Gender: "M",
+      DateOfBirth: "3 Jun",
+      CityTown: "Salem",
+      Mathematics: 62,
+      Physics: 45,
+      Chemistry: 91,
+      Total: 198,
+      Average: 66,
+    },
+    {
+      CardNo: 2,
+      Name: "Shashank",
+      Gender: "M",
+      DateOfBirth: "4 Jan",
+      CityTown: "Chennai",
+      Mathematics: 57,
+      Physics: 54,
+      Chemistry: 77,
+      Total: 188,
+      Average: 62.67,
+    },
+    {
+      CardNo: 3,
+      Name: "Rida",
+      Gender: "F",
+      DateOfBirth: "5 May",
+      CityTown: "Chennai",
+      Mathematics: 42,
+      Physics: 53,
+      Chemistry: 78,
+      Total: 173,
+      Average: 57.67,
+    },
+    {
+      CardNo: 4,
+      Name: "Ritika",
+      Gender: "F",
+      DateOfBirth: "17 Nov",
+      CityTown: "Madurai",
+      Mathematics: 87,
+      Physics: 64,
+      Chemistry: 89,
+      Total: 240,
+      Average: 80,
+    },
+    {
+      CardNo: 5,
+      Name: "Akshaya",
+      Gender: "F",
+      DateOfBirth: "8 Feb",
+      CityTown: "Chennai",
+      Mathematics: 71,
+      Physics: 92,
+      Chemistry: 84,
+      Total: 247,
+      Average: 82.33,
+    },
+    {
+      CardNo: 6,
+      Name: "Sameer",
+      Gender: "M",
+      DateOfBirth: "23 Mar",
+      CityTown: "Ambur",
+      Mathematics: 81,
+      Physics: 82,
+      Chemistry: 87,
+      Total: 250,
+      Average: 83.33,
+    },
+    {
+      CardNo: 7,
+      Name: "Aditya",
+      Gender: "M",
+      DateOfBirth: "15 Mar",
+      CityTown: "Vellore",
+      Mathematics: 84,
+      Physics: 92,
+      Chemistry: 76,
+      Total: 252,
+      Average: 84,
+    },
+    {
+      CardNo: 8,
+      Name: "Surya",
+      Gender: "M",
+      DateOfBirth: "28 Feb",
+      CityTown: "Bengaluru",
+      Mathematics: 74,
+      Physics: 64,
+      Chemistry: 51,
+      Total: 189,
+      Average: 63,
+    },
+    {
+      CardNo: 9,
+      Name: "Clarence",
+      Gender: "M",
+      DateOfBirth: "6 Dec",
+      CityTown: "Bengaluru",
+      Mathematics: 63,
+      Physics: 88,
+      Chemistry: 73,
+      Total: 224,
+      Average: 74.67,
+    },
+    {
+      CardNo: 10,
+      Name: "Kavya",
+      Gender: "F",
+      DateOfBirth: "12 Jan",
+      CityTown: "Chennai",
+      Mathematics: 64,
+      Physics: 72,
+      Chemistry: 68,
+      Total: 204,
+      Average: 68,
+    },
+    {
+      CardNo: 11,
+      Name: "Rahul",
+      Gender: "M",
+      DateOfBirth: "30 Apr",
+      CityTown: "Bengaluru",
+      Mathematics: 97,
+      Physics: 92,
+      Chemistry: 92,
+      Total: 281,
+      Average: 93.67,
+    },
+    {
+      CardNo: 12,
+      Name: "Srinidhi",
+      Gender: "F",
+      DateOfBirth: "14 Jan",
+      CityTown: "Chennai",
+      Mathematics: 52,
+      Physics: 64,
+      Chemistry: 71,
+      Total: 187,
+      Average: 62.33,
+    },
+    {
+      CardNo: 13,
+      Name: "Gopi",
+      Gender: "M",
+      DateOfBirth: "6 May",
+      CityTown: "Madurai",
+      Mathematics: 65,
+      Physics: 73,
+      Chemistry: 89,
+      Total: 227,
+      Average: 75.67,
+    },
+    {
+      CardNo: 14,
+      Name: "Sophia",
+      Gender: "F",
+      DateOfBirth: "23 July",
+      CityTown: "Trichy",
+      Mathematics: 89,
+      Physics: 62,
+      Chemistry: 93,
+      Total: 244,
+      Average: 81.33,
+    },
+    {
+      CardNo: 15,
+      Name: "Goutami",
+      Gender: "F",
+      DateOfBirth: "22 Sep",
+      CityTown: "Theni",
+      Mathematics: 76,
+      Physics: 58,
+      Chemistry: 90,
+      Total: 224,
+      Average: 74.67,
+    },
+    {
+      CardNo: 16,
+      Name: "Tauseef",
+      Gender: "M",
+      DateOfBirth: "30 Dec",
+      CityTown: "Trichy",
+      Mathematics: 87,
+      Physics: 86,
+      Chemistry: 43,
+      Total: 216,
+      Average: 72,
+    },
+    {
+      CardNo: 17,
+      Name: "Arshad",
+      Gender: "M",
+      DateOfBirth: "14 Dec",
+      CityTown: "Chennai",
+      Mathematics: 62,
+      Physics: 81,
+      Chemistry: 67,
+      Total: 210,
+      Average: 70,
+    },
+    {
+      CardNo: 18,
+      Name: "Abirami",
+      Gender: "F",
+      DateOfBirth: "9 Oct",
+      CityTown: "Erode",
+      Mathematics: 72,
+      Physics: 92,
+      Chemistry: 97,
+      Total: 261,
+      Average: 87,
+    },
+    {
+      CardNo: 19,
+      Name: "Vetrivel",
+      Gender: "M",
+      DateOfBirth: "30 Aug",
+      CityTown: "Trichy",
+      Mathematics: 56,
+      Physics: 78,
+      Chemistry: 62,
+      Total: 196,
+      Average: 65.33,
+    },
+    {
+      CardNo: 20,
+      Name: "Kalyan",
+      Gender: "M",
+      DateOfBirth: "17 Sep",
+      CityTown: "Vellore",
+      Mathematics: 93,
+      Physics: 68,
+      Chemistry: 91,
+      Total: 252,
+      Average: 84,
+    },
+    {
+      CardNo: 21,
+      Name: "Monika",
+      Gender: "F",
+      DateOfBirth: "15 Mar",
+      CityTown: "Bengaluru",
+      Mathematics: 78,
+      Physics: 69,
+      Chemistry: 74,
+      Total: 221,
+      Average: 73.67,
+    },
+    {
+      CardNo: 22,
+      Name: "Priya",
+      Gender: "F",
+      DateOfBirth: "17 Jul",
+      CityTown: "Nagercoil",
+      Mathematics: 62,
+      Physics: 62,
+      Chemistry: 57,
+      Total: 181,
+      Average: 60.33,
+    },
+    {
+      CardNo: 23,
+      Name: "Deepika",
+      Gender: "F",
+      DateOfBirth: "13 May",
+      CityTown: "Bengaluru",
+      Mathematics: 97,
+      Physics: 91,
+      Chemistry: 88,
+      Total: 276,
+      Average: 92,
+    },
+    {
+      CardNo: 24,
+      Name: "Siddharth",
+      Gender: "M",
+      DateOfBirth: "26 Dec",
+      CityTown: "Madurai",
+      Mathematics: 44,
+      Physics: 72,
+      Chemistry: 58,
+      Total: 174,
+      Average: 58,
+    },
+    {
+      CardNo: 25,
+      Name: "Geeta",
+      Gender: "F",
+      DateOfBirth: "16 May",
+      CityTown: "Chennai",
+      Mathematics: 87,
+      Physics: 75,
+      Chemistry: 92,
+      Total: 254,
+      Average: 84.67,
+    },
+    {
+      CardNo: 26,
+      Name: "JK",
+      Gender: "M",
+      DateOfBirth: "22 Jul",
+      CityTown: "Chennai",
+      Mathematics: 74,
+      Physics: 71,
+      Chemistry: 82,
+      Total: 227,
+      Average: 75.67,
+    },
+    {
+      CardNo: 27,
+      Name: "Jagan",
+      Gender: "M",
+      DateOfBirth: "4 Mar",
+      CityTown: "Madurai",
+      Mathematics: 81,
+      Physics: 76,
+      Chemistry: 52,
+      Total: 209,
+      Average: 69.67,
+    },
+    {
+      CardNo: 28,
+      Name: "Nisha",
+      Gender: "F",
+      DateOfBirth: "10 Sep",
+      CityTown: "Madurai",
+      Mathematics: 74,
+      Physics: 83,
+      Chemistry: 83,
+      Total: 240,
+      Average: 80,
+    },
+    {
+      CardNo: 29,
+      Name: "Naveen",
+      Gender: "M",
+      DateOfBirth: "13 Oct",
+      CityTown: "Vellore",
+      Mathematics: 72,
+      Physics: 66,
+      Chemistry: 81,
+      Total: 219,
+      Average: 73,
+    },
+  ],
+};
+let editor = document.getElementById("code-editor");
+editor.value = `A = 1
+B = 34
+C = A *  B * 2
+L = []
+foreach element in L {
+  L = L ++ [5]
+}
+
+C = 8
+`;
+
+function evaluateCode() {
+  console.clear();
+
+  let editor = document.getElementById("code-editor");
+  let jsCode = document.getElementById("js-code");
+  let output = document.getElementById("code-output");
+  let variables = document.getElementsByClassName("variable");
+
+  output.value = "";
+  jsCode.value = editor.value;
+
+  LIST_CONCATENATION = /(\w+)\s*=\s*\1\s*\+\+\s*(.+)/;
+  FOREACH_DEFINITION = /foreach\s*(\S+)\s+in\s+(\S+)/i;
+
+  // parsing foreach definition
+  {
+    let matched = jsCode.value.match(FOREACH_DEFINITION);
+    console.log(matched)
+    jsCode.value = jsCode.value.replace(
+      FOREACH_DEFINITION,
+      `for (${matched[1]} of ${matched[2]})`
+    );
+  }
+
+  // parsing list contactenation
+  {
+    let matched = jsCode.value.match(LIST_CONCATENATION);
+    console.log(matched)
+    jsCode.value = jsCode.value.replace(
+      LIST_CONCATENATION,
+      `${matched[1]}.push(${matched[2].replace("[", "").replace("]", "")})`
+    );
+  }
+
+  eval(jsCode);
+
+  for (let variable of variables) {
+    try {
+      output.value =
+        output.value + `${variable.value}:\n${eval(variable.value)}` + "\n\n";
+    } catch (error) {
+      output.value = error.message;
+    }
+  }
+}
+
+evaluateCode();
